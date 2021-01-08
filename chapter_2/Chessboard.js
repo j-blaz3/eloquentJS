9ea049847d4db1fb08fe5a50c405cd706cc12f23
j-blalock <justin.m.blalock@gmail.com>
2021-01-07 18:53:15 -0600

    Chessboard

---
+++ chapter_2/Chessboard.js
@@ -0,0 +1,23 @@
+// Chessboard
+
+let size = 8;
+let str = "";
+
+for (let i = 0; i < size; i++) {
+	for (let j = 0; j < size; j++) {
+		if (j % size === 0) {
+			str += "\n";
+			if (i % 2 === 0){
+				str += " ";
+			}
+		} else if (j % 2 === 0) {
+			str += " ";
+		} else {
+			str += "#";
+		}
+	}
+}
+
+console.log(str);
+console.log(`str length: ${str.length}
+size: ${size}x${size}`);
