e9a7cce8d558cca4274eaba5d797277deb75f7b2
j-blalock <justin.m.blalock@gmail.com>
2021-01-11 18:03:51 -0600

    The Sum of a Range - part 2

--- chapter_4/TheSumofaRange.js
+++ chapter_4/TheSumofaRange.js
@@ -1,12 +1,21 @@
-// Your code here.
+// The Sum of a Range
+
 function range(start, end) {
-	let s = start;
-	let e = end;
-	let result = [];
-	for (let i = s; i <= e; i++) {
+	var start = start;
+	var end = end;
+	var result = [];
+	for (var i = start; i <= end; i++) {
 		result.push(i);
 	}
 	return result;
+};
+
+function sum(arr) {
+	var tot = 0;
+  for (var i = 0; i < arr.length; i++) {
+    tot += arr[i];
+	}
+	return tot;
 };
 
 
@@ -14,5 +23,5 @@ console.log(range(1, 10));
 // → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 // console.log(range(5, 2, -1));
 // → [5, 4, 3, 2]
-// console.log(sum(range(1, 10)));
+console.log(sum(range(1, 10)));
 // → 55s
